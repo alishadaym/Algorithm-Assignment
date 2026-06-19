68883aee795edcf5167427b8a323cbcc53a8e630
alishadamia15 <alishadamiawork@gmail.com>
2026-06-20 01:12:53 +0800

    update

--- heap_sort_step.cpp
+++ heap_sort_step.cpp
@@ -133,7 +133,7 @@ void heapSort(vector<Record>& arr,
     //initial heap
     printRange(arr, startRow, endRow, outFile, "initial");
 
-    for (int i = n - 1; i > n - 6; i--) //can change to i > n - 6 -> to print first 5 iterations
+    for (int i = n - 1; i > 0; i--) //can change to i > n - 6 -> to print first 5 iterations
     {
         swap(arr[0], arr[i]);
 
